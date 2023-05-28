cd222ff45d62e98781034768deff060fa89ad9c1
Konrad Malik <konrad.malik@gmail.com>
2023-05-28 13:01:17 +0200

    http2 over h2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::{str::FromStr, sync::Arc};
+use std::{net::ToSocketAddrs, str::FromStr, sync::Arc};
 
-use hyper::{Client, Request};
+use color_eyre::eyre::eyre;
 use rustls::{Certificate, ClientConfig, KeyLogFile, RootCertStore};
+use tokio::net::TcpStream;
 use tracing::info;
 use tracing_subscriber::{filter::targets::Targets, layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -17,7 +18,7 @@ async fn main() -> color_eyre::Result<()> {
         .with(format_layer)
         .init();
 
-    info!("Setting up TLS root certificate store");
+    info!("Setting up TLS");
     let mut root_store = RootCertStore::empty();
     for cert in rustls_native_certs::load_native_certs()? {
         root_store.add(&Certificate(cert.0))?;
@@ -28,25 +29,72 @@ async fn main() -> color_eyre::Result<()> {
         .with_root_certificates(root_store)
         .with_no_client_auth();
     client_config.key_log = Arc::new(KeyLogFile::new());
+    // appliaction layer protocol negotiation: https://www.rfc-editor.org/rfc/rfc7301
+    // we need to 'tell it' we want to speak http2 and we can do it via tls if both sides speak >= TLS 1.2
+    client_config.alpn_protocols = vec![b"h2".to_vec()];
 
-    let connector = hyper_rustls::HttpsConnectorBuilder::new()
-        .with_tls_config(client_config)
-        .https_only()
-        .enable_http2()
-        .build();
+    let connector = tokio_rustls::TlsConnector::from(Arc::new(client_config));
 
-    let client = Client::builder()
-        .http2_only(true)
-        .build::<_, hyper::Body>(connector);
+    let addr = "example.org:443"
+        .to_socket_addrs()?
+        .next()
+        .ok_or_else(|| eyre!("Failed to resolve address for example.org:443"))?;
 
-    let req = Request::get("https://example.org").body(hyper::Body::empty())?;
-    info!("Performing HTTP/2 request...");
-    let res = client.request(req).await?;
-    info!("Response header: {:?}", res);
+    info!("Establishing TCP connection...");
+    let stream = TcpStream::connect(addr).await?;
 
-    info!("Reading response body...");
-    let body = hyper::body::to_bytes(res.into_body()).await?;
-    info!("Response body is {} bytes", body.len());
+    info!("Establishing TLS session...");
+    let stream = connector.connect("example.org".try_into()?, stream).await?;
+
+    info!("Establishing HTTP/2 connection...");
+    let (mut send_req, conn) = h2::client::handshake(stream).await?;
+    tokio::spawn(conn);
+
+    // for debug, notice we use the same send_req and things happen asyncronously over the same connection
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<color_eyre::Result<()>>(1);
+    for i in 0..5 {
+        let req = http::Request::builder()
+            .uri("https://example.org/")
+            .body(())?;
+        let (res, _req_body) = send_req.send_request(req, true)?;
+
+        let fut = async move {
+            let mut body = res.await?.into_body();
+            info!("{i}: received headers");
+            let mut body_len = 0;
+            while let Some(chunk) = body.data().await.transpose()? {
+                body_len += chunk.len();
+            }
+            info!("{i}: received body ({body_len} bytes)");
+            Ok::<_, color_eyre::Report>(())
+        };
+
+        let tx = tx.clone();
+        tokio::spawn(async move { _ = tx.send(fut.await).await });
+    }
+
+    drop(tx);
+    while let Some(res) = rx.recv().await {
+        res?;
+    }
+
+    // for 'real'
+    info!("Sending HTTP/2 request...");
+    let req = http::Request::builder()
+        .uri("https://example.org/")
+        .body(())?;
+    send_req = send_req.ready().await?;
+    let (res, _req_body) = send_req.send_request(req, true)?;
+    let res = res.await?;
+    info!("Got HTTP/2 response {res:?}");
+
+    let mut body = res.into_body();
+    let mut body_len = 0;
+    while let Some(chunk) = body.data().await.transpose()? {
+        body_len += chunk.len();
+    }
+    info!("Got HTTP/2 response body of {body_len} bytes");
 
     Ok(())
 }
+
