4359cd24b702ed1e7c8b7dfb4c1dd790498b4b50
Konrad Malik <konrad.malik@gmail.com>
2023-05-28 11:54:32 +0200

    chore: nix flake udpate

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Development environment for this project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
